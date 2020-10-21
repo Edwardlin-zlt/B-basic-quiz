5a704c2eae7bd72557537616d17287d9c082f1e1
lintao <lintao.zhang@thoughtworks.com>
2020-10-21 16:22:56 +0800

    [Lintao] feat: add validation to education

--- src/main/java/com/thoughtworks/basicquiz/model/Education.java
+++ src/main/java/com/thoughtworks/basicquiz/model/Education.java
@@ -4,13 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Education {
     private Long userId;
-//    TODO: Why do we use long for year?
+//    TODO: Why do we use Long type for year?
+    @NotNull
     private Long year;
+    @NotNull
+//  TODO: change this number to bytes
+    @Size(min = 1, max = 256)
     private String title;
+    @NotNull
+    @Size(min = 1, max = 4096)
     private String description;
 }
