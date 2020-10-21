51f30f019f732cc30db6f2272d285c54de386e47
lintao <lintao.zhang@thoughtworks.com>
2020-10-21 11:16:34 +0800

    [Lintao] init project

---
+++ src/test/java/com/thoughtworks/basicquiz/BasicquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.basicquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
