9d17623708bcaa128c5eaa8c600ac4e7514af17c
lintao <lintao.zhang@thoughtworks.com>
2020-10-21 12:25:11 +0800

    [Lintao] feat: add cross origin, backend connected with frontend

--- src/main/java/com/thoughtworks/basicquiz/controller/UserController.java
+++ src/main/java/com/thoughtworks/basicquiz/controller/UserController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.basicquiz.controller;
 
 import com.thoughtworks.basicquiz.model.User;
 import com.thoughtworks.basicquiz.service.UserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class UserController {
     }
 
     @GetMapping("/users")
+    @CrossOrigin
     public List<User> findAllUser() {
         return userService.findAllUser();
     }
