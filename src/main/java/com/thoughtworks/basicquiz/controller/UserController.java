81d2793bd72a8d8a36809647b0a59734ad673816
lintao <lintao.zhang@thoughtworks.com>
2020-10-21 11:45:54 +0800

    [Lintao] feat: add UserController

--- src/main/java/com/thoughtworks/basicquiz/controller/UserController.java
+++ src/main/java/com/thoughtworks/basicquiz/controller/UserController.java
@@ -1,2 +1,23 @@
-package com.thoughtworks.basicquiz.controller;public class UserController {
+package com.thoughtworks.basicquiz.controller;
+
+import com.thoughtworks.basicquiz.model.User;
+import com.thoughtworks.basicquiz.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public Collection<User> findAllUser() {
+        return userService.findAllUser();
+    }
 }
--- src/main/java/com/thoughtworks/basicquiz/service/UserService.java
+++ src/main/java/com/thoughtworks/basicquiz/service/UserService.java
@@ -2,9 +2,11 @@ package com.thoughtworks.basicquiz.service;
 
 import com.thoughtworks.basicquiz.model.User;
 import com.thoughtworks.basicquiz.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
+@Service
 public class UserService {
     private final UserRepository userRepository = new UserRepository();
 
